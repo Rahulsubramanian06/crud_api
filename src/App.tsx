f056534dc9a9e440dec06b327b8d85dcac33acd0
Rahulsubramanian06 <92245492+Rahulsubramanian06@users.noreply.github.com>
2025-05-09 18:52:38 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+export function App(){
+  const payload = {Name : "rahul"}
 
-function App() {
-  const [count, setCount] = useState(0)
 
+  async function post_api_function() {
+    const post_api:any = await axios.post("https://jsonplaceholder.typicode.com/users", payload)
+    console.log(post_api)
+   }
+   useEffect(()=> {
+    async function get_api_function() {
+      const get_api: any = await axios.get("https://jsonplaceholder.typicode.com/users")
+      console.log(get_api.data)
+  }get_api_function()})
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <button onClick={post_api_function}>submit</button>  
     </>
   )
-}
 
-export default App
+}
